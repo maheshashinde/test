84b0d1771c200e0bff0d5d814a8eadaaf4fb072d
Somavanshi <surajsomavanshi2@gmail.com>
2021-10-17 16:26:42 +0530

    Final Commit

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,30 +1,60 @@
 package calculator;
 
 class StringCalculator {
-	private final String delimiter=",|\n";
+	private String delimiter;
+	private String numbers;
+	
+	public StringCalculator() {
+		this.delimiter=",|\n";
+	}
+	public StringCalculator(String delemeter,String numbers) {
+		this.delimiter=delemeter;
+		this.numbers=numbers;
+	}
     public int add(String input) {
+    	StringCalculator calculator;
+    	String delimeter=",|\n";
     	
-    	String numbers[]=input.split(delimiter);
     	if(input.isEmpty())
     		return 0;
+    	
     	if(input.length()==1)
         return Integer.parseInt(input);
-    	else
-        return getSum(numbers);
+    	else 
+    		if(input.startsWith("//")) {
+    		String parts[]=input.split("\n",2);
+    		delimiter=parts[0].substring(2);
+    		input=parts[1];
+    		calculator=new StringCalculator(input, delimeter);
+    	}
+    		else{
+    			calculator=new StringCalculator(input, delimeter);
+    		};
+    		
+    		String numbers[]=input.split(delimiter);
+    		return getSum(numbers);
     }
 
     private int getSum(String[] numbers)
     {
+    	findDanderInput(numbers);
     	int sum=0;
     	for(String num:numbers)
     	{
-    		if(getInt(num)<0)
-    			throw new IllegalArgumentException("negative number found");
+    		if(getInt(num)>1000)
+    			continue;
     		sum+=getInt(num);
     	}
     	return sum;
     }
-    
+    private void findDanderInput(String[] numbers)
+    {
+    	for(String num:numbers)
+    	{
+    		if(getInt(num)<0)
+    			throw new IllegalArgumentException("negative number found");
+    	}
+    }
     private int getInt(String str)
     {
     	return Integer.parseInt(str);
