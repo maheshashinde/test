dfe912a0236a95ce5779202e30dbb40c3eda4d68
Somavanshi <surajsomavanshi2@gmail.com>
2021-10-17 14:31:04 +0530

    multiple number sum

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,13 +3,27 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-    	String numbers[]=input.split(",");
+    	String numbers[]=input.split(",|\n");
     	if(input.isEmpty())
     		return 0;
     	if(input.length()==1)
         return Integer.parseInt(input);
     	else
-        return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+        return getSum(numbers);
     }
 
+    private int getSum(String[] numbers)
+    {
+    	int sum=0;
+    	for(String num:numbers)
+    	{
+    		sum+=getInt(num);
+    	}
+    	return sum;
+    }
+    
+    private int getInt(String str)
+    {
+    	return Integer.parseInt(str);
+    }
 }
