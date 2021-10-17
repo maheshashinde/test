0749c75fb82962749809a9cf8bbff3f7185222b3
Somavanshi <surajsomavanshi2@gmail.com>
2021-10-17 14:15:09 +0530

    basic 4 test

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,10 +3,13 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-
-
-        System.out.println("test initials22");
-        return 0;
+    	String numbers[]=input.split(",");
+    	if(input.isEmpty())
+    		return 0;
+    	if(input.length()==1)
+        return Integer.parseInt(input);
+    	else
+        return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
     }
 
 }
